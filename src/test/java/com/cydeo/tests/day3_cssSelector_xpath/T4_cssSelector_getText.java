2a1e62aa7cb0d1dac66e9189b67acf3559b24260
omerDemirel <demirel999@yahoo.com>
2022-05-19 12:13:10 +0200

    T4_cssSelector_getText task done

---
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T4_cssSelector_getText.java
@@ -0,0 +1,29 @@
+package com.cydeo.tests.day3_cssSelector_xpath;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class T4_cssSelector_getText {
+    public static void main(String[] args) {
+//        TC #4: NextBaseCRM, locators, getText(), getAttribute() practice
+//        1- Open a chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+//        2- Go to: https://login1.nextbasecrm.com/?forgot_password=yes
+        driver.get("https://login1.nextbasecrm.com/?forgot_password=yes");
+
+//        3- Verify “Reset password” button text is as expected:
+//        Expected: Reset password
+        WebElement resetPasswordButton = driver.findElement(By.cssSelector("button[onclick=\"BX.addClass(this, 'wait');\"]"));
+        String actualResetPasswordButtonText = resetPasswordButton.getText();
+        String expectedResetPasswordButtonText = "Reset password";
+        WebDriverFactory.actualVsExpectedEquals(actualResetPasswordButtonText, expectedResetPasswordButtonText, "Reset password Button Text");
+
+//        PS: Inspect and decide which locator you should be using to locate web
+//        elements.
+//        PS2: Pay attention to where to get the text of this button from
+    }
+}
