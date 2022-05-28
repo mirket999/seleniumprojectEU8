07d62de2fd310b5e21d4a8e91135cb1959e111d6
omerDemirel <demirel999@yahoo.com>
2022-05-28 23:23:11 +0200

    week03 TestNG review done with Oscar's live session

--- src/test/java/com/cydeo/utilities/Test.java
+++ src/test/java/com/cydeo/utilities/Test.java
@@ -2,6 +2,6 @@ package com.cydeo.utilities;
 
 public class Test {
     public static void main(String[] args) {
-     WebDriverFactory.getDriver("firefox") ;
+     WebDriverFactory.getDriver("chrome") ;
     }
 }
