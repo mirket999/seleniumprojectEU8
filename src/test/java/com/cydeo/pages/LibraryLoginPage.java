6fece43a561d39f25f9a1d82752464ed1022c4f2
omerDemirel <demirel999@yahoo.com>
2022-06-16 20:24:13 +0200

    POM Practices

--- src/test/java/com/cydeo/pages/LibraryLoginPage.java
+++ src/test/java/com/cydeo/pages/LibraryLoginPage.java
@@ -1,4 +1,31 @@
 package com.cydeo.pages;
 
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LibraryLoginPage {
+    public LibraryLoginPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "inputEmail")
+    public WebElement inputUserName;
+
+
+    @FindBy(id = "inputEmail")
+    public WebElement inputPassword;
+
+
+    @FindBy(id = "inputEmail")
+    public WebElement signInButton;
+
+    @FindBy(xpath = "//div[.='This field is required.']/div")
+    public WebElement fieldRequiredMessage;
+
+    @FindBy(xpath = "//div[.='Sorry, Wrong Email or Password']")
+    public WebElement wrongEmailOrPasswordMessage;
+
+
 }
