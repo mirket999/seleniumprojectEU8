ddf2fb21e4cddcb5d00b69e02a3b581e30046b63
omerDemirel <demirel999@yahoo.com>
2022-06-18 13:23:11 +0200

    POM page creations

--- src/test/java/com/cydeo/pages/LibraryLoginPage.java
+++ src/test/java/com/cydeo/pages/LibraryLoginPage.java
@@ -6,26 +6,36 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LibraryLoginPage {
-    public LibraryLoginPage() {
+
+    //#1- initialize the driver instance and object of the class
+    public LibraryLoginPage(){
+        /*initElements method will create connection in between the current
+        driver session (instance) and the object of the current class.
+         */
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    @FindBy(id = "inputEmail")
-    public WebElement inputUserName;
 
+    //#2- use @FindBy annotation to locate web elements
 
-    @FindBy(id = "inputEmail")
+    @FindBy(xpath = "//input[@id='inputEmail']")
+    public WebElement inputUsername;
+
+    @FindBy(id = "inputPassword")
     public WebElement inputPassword;
 
-
-    @FindBy(id = "inputEmail")
+    @FindBy(xpath = "//button[.='Sign in']")
     public WebElement signInButton;
 
     @FindBy(xpath = "//div[.='This field is required.']/div")
-    public WebElement fieldRequiredMessage;
+    public WebElement fieldRequiredErrorMessage;
+
+    @FindBy(xpath = "//div[.='Please enter a valid email address.']/div")
+    public WebElement enterValidEmailErrorMessage;
 
     @FindBy(xpath = "//div[.='Sorry, Wrong Email or Password']")
-    public WebElement wrongEmailOrPasswordMessage;
+    public WebElement wrongEmailOrPasswordErrorMessage;
+
 
 
 }
